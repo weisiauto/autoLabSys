4eae4167e3b9e3c7e455e530c54fc2c65bb99430
John Wei <weizy@126.com>
2017-11-23 12:50:27 +0800

    Add adruino lib for modbus

---
+++ Arduino/ArduinoModbus/ArduinoModbus.ino
@@ -0,0 +1,23 @@
+#include "Arduino.h"
+#include "Modbus.h"
+#include "ModbusSerial.h"
+
+const int LED_COIL=13;
+const int LEDPin=13;
+ModbusSerial mb;
+
+void setup() {
+  // put your setup code here, to run once:
+//Config Modbus Serial(port,speed,byte format)
+mb.config(&Serial,9600,SERIAL_8N1);
+//slave ID 1-247
+mb.setSlaveId(1);
+mb.addCoil(LED_COIL,false);
+pinMode(LEDPin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+mb.task();
+digitalWrite(LEDPin,mb.Coil(LED_COIL));
+}
