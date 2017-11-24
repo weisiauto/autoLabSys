35a16cc88f42865485c048c41cfbe507757059ad
John Wei <weizy@126.com>
2017-11-24 17:32:17 +0800

    add modbus conf.h

---
+++ Arduino/ArduinoModbus/modbusConf.h
@@ -0,0 +1,17 @@
+/*
+    modbusConf.h - Header for Modbusconfiguration
+    Copyright (C) 2017 weizy@126.com
+    
+*/
+
+/**
+ * @file modbusConf.h
+ * @author weizy@126.com
+ * @version 1.0
+ * 
+ * @section DESCRIPTION
+ */
+#include "Arduino.h"
+#include "Modbus.h"
+
+
